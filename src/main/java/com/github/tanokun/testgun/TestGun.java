f6bdf65eabdaf18605fd52a84863ea7426f02b4e
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-12-15 22:54:09 +0900

    [add] first-commit

---
+++ src/main/java/com/github/tanokun/testgun/TestGun.java
@@ -0,0 +1,14 @@
+package com.github.tanokun.testgun;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestGun extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
